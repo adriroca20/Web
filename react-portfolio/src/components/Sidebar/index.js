d4ff3caf824e593f16c6b84ba0c77d13862ebed3
Adriroca20 <microhardquiz@gmail.com>
2022-09-22 20:24:27 +0200

    Avances portfolio

---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,30 @@
+import { Link, NavLink } from "react-router-dom"
+import "./index.scss"
+import LogoS from "../../assets/images/logo-s.png"
+import LogoSubtitle from "../../assets/images/logo_sub.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHome } from "@fortawesome/free-solid-svg-icons"
+
+
+const Sidebar =()=>(
+    <div className="nav-bar">
+        <Link className="logo" to="/">
+            <img src={LogoS} alt="logo"></img> 
+            <img className="sub-logo" src={LogoSubtitle} alt="logo"></img> 
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faUSer} color="#4d4d4e"></FontAwesomeIcon>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
+            </NavLink>
+        </nav>
+    </div>
+)
+
+
+export default Sidebar;
